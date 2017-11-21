cc78eccd02b188b6205ff5368f8a16960c4edeba
Sarah <sarahraqueld@gmail.com>
2017-11-21 13:39:56 -0300

    Update code

--- main.go
+++ main.go
@@ -8,31 +8,40 @@ import (
 )
 
 func random(min, max int) int {
-    rand.Seed(time.Now().Unix())
     return rand.Intn(max - min) + min
 }
 
 func user(channel chan string, id int){
-	computer:= <- channel
-	myrand := random(15, 30)
-	fmt.Println(strconv.Itoa(id) + " está online no " + computer + " por " + strconv.Itoa(myrand) + " minutos")
-	time.Sleep(time.Duration(myrand/10) * time.Second)
-	fmt.Println(strconv.Itoa(id) + " terminou no " + computer)
-	channel <- computer
+	select {
+    		case computer, ok := <-channel:
+        		if ok {
+				useComputer(channel, id, computer)
+			}
+		default:
+			fmt.Println("Adolescente " + strconv.Itoa(id) +" está aguardando")
+			computer:= <-channel
+			useComputer(channel, id, computer)
+	}
+	
 }
 
+func useComputer(channel chan string, id int, computer string){
+	myrand := random(2, 8)
+	fmt.Println("Adolescente " + strconv.Itoa(id) + " está online.")
+	time.Sleep(time.Duration(myrand) * time.Second)
+	fmt.Println("Adolescente " + strconv.Itoa(id) + " liberou a máquina após passar " +  strconv.Itoa(myrand) +" minutos on-line.")
+	channel <- computer
+}
 
 func main() {	
 	channel := make(chan string, 8)
 	for i := 0; i < 8; i++ {
-		channel <- "Pc " + strconv.Itoa(i)
+		channel <- strconv.Itoa(i)
 	}
-	fmt.Println("Lan House is open")
-		
 	for i := 1; i < 27; i++ {
 		go user(channel, i)
 	}
 	
 	time.Sleep(time.Duration(40) * time.Second)
-	fmt.Println("Hello, 世界")
+	fmt.Println("A lan-house está finalmente vazia e todos foram atendidos")
 }
